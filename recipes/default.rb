5d6a51e2b2aba8cdf193be68e584e359b0a0a351
Sebastian Trahm <basti@schleifi.com>
2014-12-09 21:05:17 +0100

    enable built-in msp programmer (default)
    * built-in msp programmer should be enabled by default recipe
    * bump cookbook version

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,11 @@
 node['msp430']['packages'].each do |p|
   package p
 end
+
+template "/etc/udev/rules.d/30-mspprog.rules" do
+  source "30-mspprog.rules"
+  mode "0644"
+  owner "root"
+  group "root"
+  action :create_if_missing
+end
